aa12b004f844f2439695c936327a2ee21bdfda32
Alex Aralis <alex.aralis@gmail.com>
2017-06-30 15:18:00 -0700

    add conditionally displayed buttons to the menu bar for sign in sign out and profile name.  the sign out and profile name buttons are not wired

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -5,23 +5,69 @@ import { push } from 'react-router-redux';
 
 const Menubar =
 (
-  { path, onItemClick } :
-  { path: string, onItemClick: Function }
+  {
+    path,
+    user,
+    onItemClick,
+    onSignOut,
+    onProfile,
+    } : {
+    path: string,
+    user: {},
+    onItemClick: Function,
+    onSignOut: Function,
+    onProfile: Function,
+  }
 ) =>
   <Menu pointing secondary>
-    <Menu.Item content='Home' name='/' active={path === '/'} onClick={onItemClick} />
-    <Menu.Item content='Hello' name='/hello' active={path === '/hello'} onClick={onItemClick} />
+    <Menu.Item
+      content='Home'
+      name='/'
+      active={path === '/'}
+      onClick={onItemClick} />
+
+    <Menu.Item
+      content='Hello'
+      name='/hello'
+      active={path === '/hello'}
+      onClick={onItemClick} />
+
     <Menu.Menu position='right'>
-      <Menu.Item
-        content='Sign Up'
-        name='/sign-up'
-        active={path === '/sign-up'}
-        onClick={onItemClick} />
+      {
+        !user.token
+          ? <Menu.Item
+            content='Sign Up'
+            name='/sign-up'
+            active={path === '/sign-up'}
+            onClick={onItemClick} />
+          : null
+      }
+
+      {
+        user.token
+          ? <Menu.Item
+            name={`/user/${user.email}`}
+            content={user.username}
+            onClick={onProfile} />
+          : null
+      }
+
+      {
+        user.token
+          ? <Menu.Item
+            content='Sign Out'
+            onClick={onSignOut} />
+          : null
+      }
     </Menu.Menu>
   </Menu>
 ;
 
 export default connect(
-  ({ router }) => ({ path: router.location.pathname }),
-  dispatch => ({ onItemClick: (_, { name }) => dispatch(push(name)) })
+  ({ router, user }) => ({ path: router.location.pathname, user }),
+  dispatch => ({
+    onItemClick: (_, { name }) => dispatch(push(name)),
+    onSignOut: () => {},
+    onProfile: () => {},
+  }),
 )(Menubar);
